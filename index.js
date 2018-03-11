60a23661e3e71686f88146bd0178464d9b2deab2
Stefan <stefan.tertan@yahoo.com>
2018-03-11 14:09:46 +0000

    added sequelize support to the app

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+const path = require("path");
 const express = require('express');
-const app = express();
+const Sequelize = require('sequelize');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+const env = process.env.NODE_ENV || "development";
+const conf = require(path.resolve(__dirname, "conf", env));
 
-module.exports = app;
+const sequelize = new Sequelize(
+    conf.db.database,
+    conf.db.username,
+    conf.db.password,
+    conf.db.sequelize
+);
+
+module.exports = async () => {
+    const app = express();
+    await sequelize.sync();
+
+    app.get('/', (req, res) => res.send('Hello World!'));
+
+    return app;
+};
