6990c00000c6fb72e3b081e21a20b81859cb251a
Stefan <stefan.tertan@yahoo.com>
2018-03-11 14:08:36 +0000

    added db configs

---
+++ conf/production.js
@@ -0,0 +1,13 @@
+const pkg = require("./../package");
+
+module.exports = {
+    appName: pkg.name,
+    db: {
+        uri: process.env.DATABASE_URL,
+        sequelize: {
+            dialect: "postgres",
+            operatorsAliases: false,
+            logging: false
+        }
+    }
+};
